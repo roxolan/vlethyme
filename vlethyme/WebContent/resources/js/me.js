5578bfb53069f05d688f3a6870253170008d7726
Vic on Mac <roxolanus@gmail.com>
2014-02-16 20:02:20 +0200

    meClip toggling the personal profiles buttons there and back, icon toggling, too

--- vlethyme/WebContent/resources/js/me.js
+++ vlethyme/WebContent/resources/js/me.js
@@ -4,6 +4,14 @@
 
 var baseUrl = '/me';
 
-$(document).ready(function(){
-	alert("ME Page!");
+var toggleClip = function($clip) {
+    // Toggle the clip options
+    $('ul', $clip).toggle();
+    // Toggle the caret icons
+    $('i.icon-caret-down, i.icon-caret-up', $clip).toggleClass('icon-caret-down icon-caret-up');
+};
+
+$(document).on('click', function(ev) {
+	var $clip = $(ev.target).parents('.clip-content');
+	toggleClip($clip);
 });
