8c85380a1ba569b80eaa0250302c485fbe58719c
Vic on Mac <roxolanus@gmail.com>
2014-02-26 14:58:06 +0200

    lhnavigation-pages open on lhnavigation-menu clicks

--- vlethyme/WebContent/resources/js/me.js
+++ vlethyme/WebContent/resources/js/me.js
@@ -1,10 +1,36 @@
 var baseUrl = '/me';
 var $rootel = $('#');
 
-var setUpNavigation = function() {
-	$(document).on('click', '#lhnavigation-navigation ul li', function(ev) {
-		alert("it clicks!");
+var defaultPage = function(baseUrl){
+	$(document).ready(function(){
+		$('#lhnavigation-navigation ul #dashboard').addClass('active');
+		$('#lhnavigation-page #dashboard').show();
 	});
 };
 
-setUpNavigation();
+
+
+var renderPage = function(element) {
+	var selmenu = '#lhnavigation-navigation ul #' + element;
+	var selpage = '#lhnavigation-page #' + element;
+	
+	$('#lhnavigation-navigation ul li').removeClass('active');
+	$('#lhnavigation-page > div').hide();
+	$(selpage).show();
+	$(selmenu).addClass('active');
+};
+
+var setUpNavListeners = function(baseUrl) {
+	$(document).on('click', '#lhnavigation-navigation ul li', function(ev) {
+		if (1 == 1) {
+			var elem = $(this).attr('id');
+			renderPage(elem);
+		}
+		ev.preventDefault();
+	});
+};
+
+defaultPage(baseUrl);
+setUpNavListeners(baseUrl);
+
+
